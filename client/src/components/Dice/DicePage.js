913aa324dcdf3fe4f65ec4171e5a529d47db4433
arhamHumayun <44730768+arhamHumayun@users.noreply.github.com>
2019-08-27 23:06:55 -0600

    Can now remove dice component on dicePage

--- client/src/components/Dice/DicePage.js
+++ client/src/components/Dice/DicePage.js
@@ -4,6 +4,7 @@ import ListGroup from 'react-bootstrap/ListGroup'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button';
 
 export default class DicePage extends Component {
 
@@ -11,14 +12,43 @@ export default class DicePage extends Component {
     super(props);
     this.state = {
       items: [
-        100, 20, 12, 10, 8, 6, 4, 3, 2
+        {diceSize: 100, id: 1001},
+        {diceSize: 20, id: 201}, 
+        {diceSize: 6, id: 61}, 
+        {diceSize: 2, id: 21},  
       ]
     }
+    this.removeItem = this.removeItem.bind(this)
+    this.addItem = this.addItem.bind(this)
+  }
+
+  addItem(diceSize, id) {
+
+  }
+  
+  removeItem(e) {
+    const newItems = this.state.items.filter(item => item.id !== e)
+    this.setState({items: newItems})
   }
 
   render() {
-    const listItems = this.state.items.map((diceSize) =>
-      <ListGroup.Item> <Dice diceSize={diceSize}/> </ListGroup.Item>
+    const listItems = this.state.items.map((item) =>
+      <ListGroup.Item> 
+      <Container style={{
+        left: '0px',
+        position: 'left',
+        display: 'contents'                
+        }}>
+        <Row>
+          <Col sm={11} style={{padding: '0'}}>
+            <Dice diceSize={item.diceSize}/>
+          </Col>
+          <Col sm={1} style={{padding: '0'}}>
+            <Button onClick={() => this.removeItem(item.id)}>X</Button>  
+          </Col>
+        </Row>
+      </Container> 
+      </ListGroup.Item>
     );
     console.log(listItems)
        return (
